7d5716c335e7bf946d2b87250778d7c6f8b694d4
neel2435 <neeldoshi2435@gmail.com>
2023-07-28 21:25:10 -0700

    cleaning errors, files to be connected

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,5 @@
 <script>
-  import { onMount } from "svelte";
-  import { _getFileFromS3 } from "./page.server.ts"; // Assuming the file is named page.server.ts
-
-  const bucketName = "peacho-bucket";
-  const objectKey = "2fb31edca72c058c5e3d6453abf11fc8c97bc81873bc1a73700dfe7f152aabc8.html";
-
-  _getFileFromS3(bucketName, objectKey)
-  .then((fileContents) => {
-    console.log("File contents:", fileContents);
-    // Here, you can process and use the HTML contents as needed.
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+ 
 
 </script>
 
--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,22 +1,28 @@
-import express from "express";
-import { getFileFromS3 } from "./page.server"; 
+import http from "http";
+import { _getFileFromS3 } from "./+page.server";
 
-const app = express();
 const port = 3000;
 
-app.get("/api/getFileFromS3", async (req, res) => {
-  const bucketName = "your-bucket-name"; 
-  const objectKey = "your-html-file.html"; 
+const server = http.createServer(async (req, res) => {
+  if (req.url === "/api/_getFileFromS3" && req.method === "GET") {
+    const bucketName = "peacho-bucket";
+    const objectKey = "cfd63f21ef797ab05dc2a65bc1ef3fd4493e5d4b34fe641ccbdd3291e9abf655.html";
 
-  try {
-    const fileContents = await getFileFromS3(bucketName, objectKey);
-    res.send(fileContents);
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).send("Error retrieving file from S3");
+    try {
+      const fileContents = await _getFileFromS3(bucketName, objectKey);
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(fileContents);
+    } catch (error) {
+      console.error("Error:", error);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Error retrieving file from S3");
+    }
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
   }
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
