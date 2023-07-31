450681e2ad17569f628db6d77ff36aa8da3b9849
neel2435 <neeldoshi2435@gmail.com>
2023-07-30 21:23:13 -0700

    separated fxns in server

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,7 +3,7 @@
 declare global {
 	namespace App {
 		interface Platform {
-			env: {
+			env?: {
 				R2_BUCKET: "peacho-bucket";
 				R2_ACCESS_KEY: "5872322597758bf928a73b8c7ec37694";
 				R2_ACCOUNT_ID: "b0b1803d133ab70e6f0ea954c2a10b04";
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,18 @@
 /** @type {import('./$types').PageServerLoad} */
+// import { R2_BUCKET } from '$env/static/private';
+
 
 console.log("in page.server")
 
 export async function load({platform}) {
+  console.log("attempting html load");
+  let html = await getHTML(platform);
+  console.log("html stored successfully");
+  return html;
+}
+
+
+ async function getHTML(platform) {
   console.log("in /+page.server.ts (getHTML)");
   const obj = await platform.env.R2_BUCKET.get('2fb31edca72c058c5e3d6453abf11fc8c97bc81873bc1a73700dfe7f152aabc8.html');
   console.log("get html from R2");
