c40fbcc1c33521db39584895e0930daa777776d4
neel2435 <neeldoshi2435@gmail.com>
2023-08-05 02:45:39 -0700

    ?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -49,7 +49,7 @@
 
 <body>
     <div class="container">
-    <form method="POST" action="/verify">
+    <form method="POST" action="?/verify">
         <label>
             <input name="url" type="url" class="textbox" placeholder="Enter URL Here...">
         </label>
