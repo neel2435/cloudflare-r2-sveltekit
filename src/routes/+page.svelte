86b98c3494fafdf325832843c9d32c57b5ae3ff4
neel2435 <neeldoshi2435@gmail.com>
2023-08-05 00:46:48 -0700

    style on landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,53 @@
-<script>
-//     /** @type {import('./$types').PageData} */
-//     export let data;
-//     console.log(data);
-//     let reconstructedString = ''; // Initialize an empty string
 
-//     for (const charPosition in data) {
-//         if (data.hasOwnProperty(charPosition)) {
-//             reconstructedString += data[charPosition]; // Concatenate the character
-//         }
-//     }
-
-// console.log(reconstructedString);
-</script>
-
-
-<!-- {@html reconstructedString} -->
-<li><a href="/retrieve">Go to page 2</a></li>
+<title>Cozy Textbox</title>
+<style>
+  body {
+    font-family: Arial, sans-serif;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    margin: 0;
+    background-color: #f0f0f0;
+  }
+  
+  .container {
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  .textbox {
+    width: 300px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin-bottom: 10px;
+    font-size: 16px;
+  }
+  
+  .button {
+    background-color: #47a7f5;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+  
+  .button:hover {
+    background-color: #368dd0;
+  }
+</style>
+<body>
+<div class="container">
+  <input type="text" class="textbox" placeholder="Enter text...">
+  <button class="button"><a href="/retrieve">Submit</a></button>
+</div>
+</body>
+<a href="/retrieve">Go to page 2</a>
