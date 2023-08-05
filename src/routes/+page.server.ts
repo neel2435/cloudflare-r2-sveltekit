f7548a73f25f6c636d232b63c7f9ff6edefd0bef
neel2435 <neeldoshi2435@gmail.com>
2023-08-04 23:02:33 -0700

    await

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -16,7 +16,7 @@ export async function load({platform}) {
   console.log("in /+page.server.ts (getHTML)");
   const html = await platform.env.R2_BUCKET.get('2fb31edca72c058c5e3d6453abf11fc8c97bc81873bc1a73700dfe7f152aabc8.html');
   console.log("get html from R2");
-  const obj = html.text();
+  const obj = await html.text();
 
   if (obj === null) {
     return null;
