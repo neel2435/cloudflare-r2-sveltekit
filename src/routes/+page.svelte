2507e311a0fc7b7c6aaa3883f90a3d5071f7079b
neel2435 <neeldoshi2435@gmail.com>
2023-08-04 23:48:33 -0700

    array shenanigans

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,14 @@
     /** @type {string} */
     export let data;
     console.log(data);
+    let newArray = []; // Array to store characters
+    for (let i = 0; i < data.length; i++) {
+         newArray.push(data[i]); // Push each character into the array
+    }
+
+    let reconstructedString = newArray.join(''); // Join the array into a single string
+    console.log(reconstructedString);
 </script>
 
 
-{@html data}
+{@html reconstructedString}
