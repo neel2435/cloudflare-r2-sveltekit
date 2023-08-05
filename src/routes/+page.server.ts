2b0a8e561acc658acb18b08cc5b7e4847ab071e0
neel2435 <neeldoshi2435@gmail.com>
2023-08-05 14:31:28 -0700

    a

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -21,7 +21,8 @@ export const actions = {
         
         const response = await fetch(url);
         const html = await response.text();
-        console.log(html);
+        const htmlNoStyle = html.replace(/<style\b[^<]*(?:(?!<\/style>)<[^<]*)*<\/style>/gi, '');
+        console.log(htmlNoStyle);
         // const htmlWithoutStyles = stripStyles(html);
       
         // const dom = new JSDOM(htmlWithoutStyles, { url });
@@ -38,7 +39,7 @@ export const actions = {
 
       return {
         success: true,
-        html: url,
+        html: html,
         hash: hashHex
       }
   }
