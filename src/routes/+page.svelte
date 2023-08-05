64ff17604b913a64f1db609bcc3aa5b25b10a86f
neel2435 <neeldoshi2435@gmail.com>
2023-08-05 01:06:44 -0700

    d

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,46 @@
 <style>
   body {
-    background-color: #000000;
-  }
-  
-  .container {
-    background-color: #ffecd7;
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-  
-  .textbox {
-    width: 300px;
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    margin-bottom: 10px;
-    font-size: 16px;
-    justify-content: center;
-    align-items: center;
-  }
-  
-  .button {
-    background-color: #47a7f5;
-    color: #fff;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: background-color 0.3s ease;
-    justify-content: center;
-    align-items: center;
-  }
-  
-  .button:hover {
-    background-color: #368dd0;
-  }
+      font-family: Arial, sans-serif;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-height: 100vh;
+      margin: 0;
+      background-color: #f0f0f0;
+    }
+    
+    .container {
+      background-color: #fff;
+      padding: 20px;
+      border-radius: 10px;
+      box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    
+    .textbox {
+      width: 300px;
+      padding: 10px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      margin-bottom: 10px;
+      font-size: 16px;
+    }
+    
+    .button {
+      background-color: #47a7f5;
+      color: #fff;
+      padding: 10px 20px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      transition: background-color 0.3s ease;
+    }
+    
+    .button:hover {
+      background-color: #368dd0;
+    }
 </style>
 <body>
 <div class="container">
