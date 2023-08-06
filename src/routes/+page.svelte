a9544ec595a667ef9ad22de261619925ef151d4a
neel2435 <neeldoshi2435@gmail.com>
2023-08-05 23:22:41 -0700

    l

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,8 @@
 	body {
       font-family: Arial, sans-serif;
       display: flex;
-      /*justify-content: center;*/
-      align-items: center;
+      justify-content: center;
+      /* align-items: center; */
       min-height: 100vh;
       margin: 0;
       background-color: #f0f0f0;
