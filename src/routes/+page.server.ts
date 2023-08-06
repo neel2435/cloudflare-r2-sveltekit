d10ed7a47fa108e2aee2b0844b3c94ce3b05cc3c
neel2435 <neeldoshi2435@gmail.com>
2023-08-05 21:46:33 -0700

    a

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,7 @@
 import {DOMParser} from "linkedom"
 import { Readability } from '@mozilla/readability';
 //import * as fs from 'fs';
-
+/** @type {import('./$types').PageServerLoad} */
 /** @type {import('./$types').Actions} */
 export const actions = {
   verify: async ({request, platform }) => {
@@ -11,45 +11,39 @@ export const actions = {
       console.log(url);
       //fix this part
       
-        const encodedJson = new TextEncoder().encode(url);
+      const encodedJson = new TextEncoder().encode(url);
 
 
-        console.log("create sha256 hash");
-        const myDigest = await crypto.subtle.digest({ name: 'SHA-256' }, encodedJson);
-        const hashArray = Array.from(new Uint8Array(myDigest));
-        const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-        console.log(hashHex);
+      console.log("create sha256 hash");
+      const myDigest = await crypto.subtle.digest({ name: 'SHA-256' }, encodedJson);
+      const hashArray = Array.from(new Uint8Array(myDigest));
+      const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+      console.log(hashHex);
         
-        const response = await fetch(url);
-        const html = await response.text();
-        //const htmlNoStyle = html.replace(/<style\b[^<]*(?:(?!<\/style>)<[^<]*)*<\/style>/gi, '');
-        
-        const domParser = new DOMParser();
-        const document = domParser.parseFromString(html, 'text/html');
+        //check R2
+      const obj = await getObject(platform, hashHex)
+      if (obj) {
+        return {
+          success: true,
+          html: JSON.stringify(obj),
+          hash: hashHex,
+          title: obj?.title,
+          content: obj?.content
+        };
+      }
 
-        //const readabilitydocument = document as Document;
+      const response = await fetch(url);
+      const html = await response.text();
+      //const htmlNoStyle = html.replace(/<style\b[^<]*(?:(?!<\/style>)<[^<]*)*<\/style>/gi, '');
         
-        const reader = new Readability(document);
-        const article = reader.parse();
-        console.log(article?.title);
-        console.log(article?.content);
-        
-        
-        
-        // console.log(htmlNoStyle);
-        // const htmlWithoutStyles = stripStyles(html);
+      const domParser = new DOMParser();
+      const document = domParser.parseFromString(html, 'text/html');
+       
+      const reader = new Readability(document);
+      const article = reader.parse();
+      console.log(article?.title);
       
-        // const dom = new JSDOM(htmlWithoutStyles, { url });
-        // const reader = new Readability(dom.window.document);
-        // const article: ReadabilityResult = reader.parse();
-      
-        // //const filePath = `${hash}.html`;
-      
-        // const formattedHtml = createFormattedHtml(article);
-
-        // console.log('Reader view stored successfully.');
-      
-      
+      await putObject(platform, hashHex, {title: article?.title, content: article?.content})
 
       return {
         success: true,
@@ -61,50 +55,22 @@ export const actions = {
   }
 };
 
-interface ReadabilityResult {
-  title: string;
-  content: string;
+async function getObject(platform, hash: string) {
+  console.log("in /+page.server.ts (getHTML)");
+  const jsonString = await platform.env.R2_BUCKET.get(hash);
+  console.log("get html from R2");
+  console.log(jsonString);
+
+  if (jsonString === null) {
+    return null;
+  }
+
+  console.log("html retrieved");
+  return JSON.parse(jsonString);
 }
 
 
-// function createFormattedHtml(article: ReadabilityResult): string {
-//   const dom = new JSDOM('');
-//   const document = dom.window.document;
-
-//   const contentElement = document.createElement('div');
-//   contentElement.innerHTML = article.content;
-
-//   const imgElements = contentElement.querySelectorAll('img');
-
-//   imgElements.forEach((imgElement) => {
-//     const parent = imgElement.parentNode;
-//     if (parent && parent.nodeName.toLowerCase() === 'p') {
-//       parent.parentNode.insertBefore(imgElement, parent);
-//     }
-//   });
-
-//   return `
-//     <!DOCTYPE html>
-//     <html lang="en">
-//       <head>
-//         <meta charset="UTF-8">
-//         <title>${article.title}</title>
-//         <link href='https://fonts.googleapis.com/css?family=Lexend Deca' rel='stylesheet'>
-//       </head>
-//       <body>
-//         <h1>${article.title}</h1>
-//         ${contentElement.innerHTML}
-//       </body>
-//       <style>
-//         body {
-//           padding: 0 28%;
-//           text-align: center;
-//           font-family: 'Lexend Deca', sans-serif;
-//           line-height: 1.6;
-//         }
-//       </style>
-//     </html>
-//   `;
-// }
-
-  
+async function putObject(platform, hash:string, article) {
+  const articleJson = JSON.stringify(article);
+  await platform.env.R2_BUCKET.put(hash, articleJson)
+}
